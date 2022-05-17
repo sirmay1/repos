45ce37f86b707f780408cd296fbdacff9122d27e
william castro <william.castro2000@outlook.com>
2022-05-17 08:31:44 -0400

    The Odin Project

---
+++ repos/Odin-Recipes/script.js
@@ -0,0 +1,9 @@
+
+
+author.addEventListener('hover', () => {
+    const author = document.querySelector('.author');
+
+author.style = "cursor: pointer; color: crimson; text-align: center; font-size: 3rem";
+
+document.body.append(author);
+});
